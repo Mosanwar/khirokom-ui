760e4a1ddf03b94529cb3ccefe153a50478929e2
Ahmed Fares <ahmed.fares90@gmail.com>
2022-06-25 15:15:23 -0400

    added bootstrap and required css (#1)
    * added bootstrap and required css
    
    * remove un needed code

---
+++ src/markup/pages/MainPage1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainPage1 extends Component {
+    render() {
+        return (
+            <h1>hello from Main Page !</h1>
+        )
+    }
+}
+
+export default MainPage1;
