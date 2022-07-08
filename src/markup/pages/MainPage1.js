88b91f5bf9175fbdc9fbdb732a2faa5aa806e23e
ahmedfares <ahmed.fares90@gmail.com>
2022-07-08 17:38:21 -0400

    test the pipeline

--- src/markup/pages/MainPage1.js
+++ src/markup/pages/MainPage1.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MainPage1 extends Component {
-    render() {
-        return (
-            <h1>hello from Main Page ! - Routing & Bootstrap Running!</h1>
-        )
-    }
+  render() {
+    return <h1>hello from Main Page - Routing & Bootstrap Running</h1>;
+  }
 }
 
 export default MainPage1;
